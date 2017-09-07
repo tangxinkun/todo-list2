cc7df615592f8b823c587cd11c0d70482a59af49
tangxinkun <517846575@qq.com>
2017-09-07 16:50:37 +0800

    test2
    getProject
    getTask test

--- twosteo.js
+++ twosteo.js
@@ -1,13 +1,87 @@
+
+
 function clickProject(element) {
 
-   // alert(element);
-    oDiv=document.getElementById("project-list");
-   cancelActive(oDiv,"")
-    addClass(element,"active")
+    // alert(element);
+    oDiv = document.getElementById("project-list");
+    cancelActive(oDiv, "")
+    addClass(element, "active");
     //element.className="active"
-
-
+    //var oInit=new initStorage();
+    //alert(typeof oInit);
+    //alert(oInit.oProject)
+    oProject=getProject(); //获得所有的project
+    //oTask   =getTask();    //获得所有的task
+    alert(typeof oProject);
+    alert(oProject);
+    //alert(oTask[0].id);
 }
+
+    function initStorage() {
+
+
+        var localStorage = new Object();
+        localStorage.project = 0;
+        localStorage.task = 0;
+
+        var projectJson = [{
+            "id": 0,
+            "name": "学习前端",
+            "child": [0, 1]
+        }, {
+            "id": 1,
+            "name": "娱乐",
+            "child": [2]
+        }];
+
+        var taskJson = [{
+            "id": 0,
+            "parent": 0,
+            "finish": false,
+            "title": "学习Bootstrap",
+            "content": "学习Bootstrap官网教程\n写Bootstrap demo",
+            "due-date": null
+        }, {
+            "id": 1,
+            "parent": 0,
+            "finish": false,
+            "title": "完成Todolist",
+            "content": "这里还是什么内容也没有",
+            "due-date": null
+        }, {
+            "id": 2,
+            "parent": 1,
+            "finish": false,
+            "title": "看电影",
+            "content": "这里也是什么内容也没有",
+            "due-date": null
+        }
+        ];
+
+        localStorage.project = JSON.stringify(projectJson);//stringify()用于从一个对象解析出字符串
+
+        localStorage.task = JSON.stringify(taskJson);//stringify()用于从一个对象解析出字符串
+        alert(localStorage.project);
+        alert(localStorage.task);
+    }
+
+    function getProject() {
+        initStorage();
+        //var sP=localStorage.project.reverse();
+
+        return localStorage.project
+
+    }
+    function getTask() {
+        initStorage();
+        return  JSON.parse(localStorage.task.reverse())
+
+    }
+
+        //return alert("1");
+        //return alert(2);
+
+
 function addClass(elem,newclassname) {
 
    // alert(newclassname);
@@ -27,3 +101,5 @@ function cancelActive(ele,oldclassname) {
    }
   //just github
 }
+
+//新建project列表和task列表
